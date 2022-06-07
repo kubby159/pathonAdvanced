1d535322df41210e179893ec8d0b3ca1352b9b8e
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-06-07 23:59:45 +0900

    advanced

---
+++ myvenv/include/Chapter02/01.문자열다루기.py
@@ -0,0 +1,28 @@
+# 1. replace
+# 문자열 교체
+
+a  = '오늘 날씨는 흐림 입니다'.replace('흐림','맑음')
+print(a)
+
+# 2. find
+# 문자열 찾기, false면 -1 반환
+b = "Hello World".find('World')
+print(b)
+
+# 3. split
+# 문자열 분리
+c = '나이키신발 265 X1212 78000'.split()
+print(c) # ['나이키신발', '265', 'X1212', '78000']
+
+d = '나이키신발:265:X1212:78000'.split(':')
+print(d) # ['나이키신발', '265', 'X1212', '78000']
+
+
+# 4. strip
+# 문자열 공백 제거
+e = '        Yeah       '.lstrip()
+print(e)
+f = '        Yeah       '.rstrip()
+print(f)
+g = '        Yeah       '.strip()
+print(g)
---
+++ myvenv/include/Chapter02/02.문자열포메팅.py
@@ -0,0 +1,9 @@
+# 문자열 포메팅이 없다면?
+# 기준님 수강기간이 7일 남았습니다.
+
+name = '기준'
+duration = '7'
+
+message = name + '님 수강기간이 ' + duration +'일 남았습니다.'
+
+print(message)
