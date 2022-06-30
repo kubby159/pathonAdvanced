02e0e7361a3cb54d08817db10dcdcd2bb3d0679b
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-06-30 23:44:52 +0900

    클로저 복기

--- myvenv/include/Chapter08/02.클로저.py
+++ myvenv/include/Chapter08/02.클로저.py
@@ -20,21 +20,22 @@ func()
 # 2) 외부 함수의 변수를 참조해야한다.
 # 3) 외부 함수가 내부 함수를 반환해야한다.
 
-def greeting(name,age,gender):
+def greeting(name,age, gender):
     def inner():
-        print(f'{name} 님 안녕하세요')
-    
-    return inner()
+        print(f'{name} 님 안녕하세요.')
+        print(f'{age} 은 나이입니다')
+        print(f'{gender} 입니다.')
+    return inner
 
-closure = greeting('나미',27,'나미')
+
+closure = greeting('나미', 27, 'female')
 closure()
 
-print(type(closure.__clousre__[0].cell_contents))
+print(closure.__closure__[0].cell_contents) # cell_contents' == '나미' 
 
 
-for i in closure__clousre__:
-    print(i.cell.contents)
-
+for x in closure.__closure__:
+    print(f' cell_contents: {x.cell_contents}')
 
 # 전역변수를 사용해서 대체가 가능하다.
 # 전역변수 사용을 최소화 하기는 것이다 좋다(네이밍문제, 스코프문제)
